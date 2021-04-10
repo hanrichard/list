11de2f230e8647cf7c38c0b213734ba14df41ae1
richardhan <hxy.314@gmail.com>
2021-04-10 16:11:58 +1000

    feat: add fetch list func

--- src/axios-api.js
+++ src/axios-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const API_KEY = 'b4ef34a1';
-export const AXIOS_API = axios.create({
+export const axiosApi = axios.create({
   baseURL: 'http://www.omdbapi.com/',
 });
--- src/container/App.js
+++ src/container/App.js
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { API_KEY, axiosApi } from '../axios-api';
 
-const App = () => (
-  <div>
-    test
-  </div>
-);
+const AppWrapper = styled.div`
+  display: flex
+`;
+
+const App = () => {
+  const [movieData, setMovieData] = useState({ moviesResults: [], totalResults: 0 });
+  const [searchError, setSearchError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [querykeyWord, setQuerykeyWord] = useState();
+  const [queryPage, setQueryPage] = useState(1);
+  const [movieID, setMovieID] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    setSearchError(null);
+    setMovieData({ moviesResults: [], totalResults: 0 });
+
+    if (querykeyWord) {
+      axiosApi.get(`?s=${querykeyWord}&apikey=${API_KEY}&page=${queryPage}`)
+        .then((response) => {
+          const {
+            data: {
+              Search, totalResults, Response, Error,
+            },
+          } = response;
+          if (Response === 'False') {
+            setSearchError(Error);
+          } else {
+            setMovieData({
+              moviesResults: Search,
+              totalResults,
+            });
+          }
+        }).catch(({ message }) => {
+          setSearchError(message);
+          setLoading(false);
+        });
+    }
+  }, [querykeyWord, queryPage]);
+
+  useEffect(() => {
+    if (!querykeyWord) {
+      setMovieData({ moviesResults: [], totalResults: 0 });
+      setSearchError('');
+    }
+
+    setQueryPage(1);
+  }, [querykeyWord]);
+
+  console.log('xxxx', movieData);
+  return (
+    <AppWrapper>
+      <div>siderbar</div>
+      <div>main</div>
+    </AppWrapper>
+  );
+};
 
 export default App;
