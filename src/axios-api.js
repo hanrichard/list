0025439f0f55124e8968fb3bf68b9d2179087931
richardhan <hxy.314@gmail.com>
2021-04-12 21:55:49 +1000

    feat(update api key): update api key

--- src/axios-api.js
+++ src/axios-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_KEY = 'b4ef34a1'; // b4ef34a1 ae3e09af
+export const API_KEY = 'ae3e09af';
 export const axiosApi = axios.create({
   baseURL: 'http://www.omdbapi.com/',
 });
